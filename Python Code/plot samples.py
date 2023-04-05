a50bd618aaf544d65344183af9906465e2ad8b14
Paul D. Rosero-Montalvo <puldavid87@gmail.com>
2023-04-05 12:12:34 +0200

    Add files via upload

---
+++ Python Code/plot samples.py
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*-
+"""
+Visualize 3 images per label
+"""
+
+
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+import random
+import os
+import random
+import shutil
+
+train_dir="C:/Users/paur/Documents/Tomato_leaves_illness_detection/DATASET_1/tomato10/train"
+
+def view_images(target_dir, target_class, num):
+  target_path=target_dir + "/" + target_class
+  file_names = os.listdir(target_path)
+  target_images = random.sample(file_names, num)
+  plt.figure(figsize=(15, 6))
+  for i, img in enumerate(target_images):
+    img_path = target_path + "/" + img
+    plt.subplot(1, num, i+1)
+    plt.imshow(mpimg.imread(img_path))
+    plt.title(target_class)
+    plt.axis("off")
+
+labels=[
+"Tomato___Tomato_Yellow_Leaf_Curl_Virus",
+"Tomato___Spider_mites Two-spotted_spider_mite",
+"Tomato___Leaf_Mold",
+"Tomato___Late_blight",
+"Tomato___Target_Spot",
+"Tomato___Bacterial_spot",
+"Tomato___Early_blight",
+"Tomato___Septoria_leaf_spot",
+"Tomato___healthy",
+"Tomato___Tomato_mosaic_virus"
+]
+
+for i in labels:
+  view_images(target_dir=train_dir ,target_class=i,num=3)
+
+random_image = random.sample(os.listdir(train_dir+"/"+"Tomato___Septoria_leaf_spot/"),1)
+ # Read in the image and plot it using matplotlib
+img = mpimg.imread(train_dir +"/"+"Tomato___Septoria_leaf_spot/"  + random_image[0])
+print(f"Image shape: {img.shape}")
+
+img = mpimg.imread("C:/Users/paur/Documents/Tomato_leaves_illness_detection/Carlos1.JPEG")
+print(f"Image shape: {img.shape}")
